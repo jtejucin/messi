f0275cc348cfa2fd0662164a96543ff77c15fd35
juanchito <juanmacardona701@gmail.com>
2022-02-22 08:13:46 -0500

    listas

--- wos.py
+++ wos.py
@@ -1,12 +1,12 @@
+from pickle import APPENDS
+
+
 numbers = [10, 5 , 7 , 2 ,1 ]
 print (numbers)
 
 numbers [0] = 111
 print (numbers) 
 
-for i in numbers:
-    print (i)
-    
 numbers [4] = numbers[0]
 print (numbers)
 
@@ -14,3 +14,12 @@ print (numbers)
 print (numbers[2])
 
 print(len(numbers))
+
+numbers.append(8.9)
+print (numbers)
+
+deportesx =[]
+deportesx.append(input("deporte favorito"))
+
+del numbers[1]
+print (numbers)
